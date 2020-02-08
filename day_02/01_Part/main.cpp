a2691dedd575bda78d384b389d4fc60c51e6cebb
Keepen <46278766+Keepen@users.noreply.github.com>
2020-02-08 22:46:02 +0800

    创建界面

---
+++ day_02/01_Part/main.cpp
@@ -0,0 +1,14 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
+
+//利用ui进行设计
+//利用可视化编辑来替代之前的函数操作
