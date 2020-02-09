eeaf584a7e774f63bac43808051f01114048eed3
Keepen <46278766+Keepen@users.noreply.github.com>
2020-02-09 21:08:49 +0800

    常用控件

--- day_02/01_Part/mainwindow.cpp
+++ day_02/01_Part/mainwindow.cpp
@@ -1,11 +1,63 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
+#include <QCompleter>
+#include <QStringList>
+#include <QMovie>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //获取内容
+    QString str = ui->lineEdit->text();
+    qDebug() << str;
+
+    //设置内容
+    ui->lineEdit->setText("987654");
+
+    //设置内容间隙
+    ui->lineEdit->setTextMargins(15,0,0,0);
+
+    //设置内容显示方式
+    //ui->lineEdit->setEchoMode(QLineEdit::Password);   //以密码模式显示
+    QStringList list;
+    list << "hello" << "How are you ";
+    QCompleter* com  = new QCompleter(list, this);
+    com->setCaseSensitivity(Qt::CaseInsensitive);
+    ui->lineEdit->setCompleter(com);
+
+    //QLabel
+    ui->labelText->setText("123");
+
+    ui->labelimage->setPixmap(QPixmap("://image/123.png"));
+    //设置图片自动适应label大小
+    ui->labelimage->setScaledContents(true);
+
+    //设置动画
+    QMovie* myMovie = new QMovie("://image/123.png");
+    ui->labelGif->setMovie(myMovie);
+
+    //启动动画
+    myMovie->start();
+    ui->labelGif->setScaledContents(true);
+
+    //设置网址
+    ui->labelUrl->setText("<h1><a href=\"https://www.baidu.com\">百度</a></h1>");
+    ui->labelUrl->setScaledContents(true);
+    //设置打开外部链接
+    ui->labelUrl->setOpenExternalLinks(true);
+
+    //数码管
+    ui->lcdNumber->display(123);
+
+    //进度条
+    ui->progressBar->setMinimum(0);   //设置最小值
+    ui->progressBar->setMaximum(100); //设置最大值
+    ui->progressBar->setValue(20);   //设置当前值
 }
 
 MainWindow::~MainWindow()
@@ -16,5 +68,12 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
+
+}
+
+void MainWindow::on_change_clicked()
+{
+    //static int i = 0;
+    //ui->stackedWidget->setCurrentIndex(++i % 3);
 
 }
--- day_02/build-01_Part-Desktop_Qt_5_14_0_MinGW_32_bit-Debug/ui_mainwindow.h
+++ day_02/build-01_Part-Desktop_Qt_5_14_0_MinGW_32_bit-Debug/ui_mainwindow.h
@@ -10,12 +10,19 @@
 #define UI_MAINWINDOW_H
 
 #include <QtCore/QVariant>
+#include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QDialogButtonBox>
+#include <QtWidgets/QLCDNumber>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QProgressBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
-#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -23,38 +30,105 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
+    QAction *actionnew;
     QWidget *centralwidget;
-    QWidget *verticalLayoutWidget;
-    QVBoxLayout *verticalLayout;
     QPushButton *pushButton;
+    QDialogButtonBox *buttonBox;
+    QWidget *widget;
+    QPushButton *pushButton_8;
+    QPushButton *pushButton_9;
+    QPushButton *pushButton_10;
+    QPushButton *pushButton_11;
+    QLineEdit *lineEdit;
+    QComboBox *comboBox;
+    QLabel *labelText;
+    QLabel *labelimage;
+    QLabel *labelGif;
+    QLabel *labelUrl;
+    QPushButton *change;
+    QLCDNumber *lcdNumber;
+    QProgressBar *progressBar;
     QMenuBar *menubar;
+    QMenu *menu;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(800, 600);
+        MainWindow->resize(996, 652);
+        actionnew = new QAction(MainWindow);
+        actionnew->setObjectName(QString::fromUtf8("actionnew"));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
-        verticalLayoutWidget = new QWidget(centralwidget);
-        verticalLayoutWidget->setObjectName(QString::fromUtf8("verticalLayoutWidget"));
-        verticalLayoutWidget->setGeometry(QRect(160, 50, 160, 80));
-        verticalLayout = new QVBoxLayout(verticalLayoutWidget);
-        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
-        verticalLayout->setContentsMargins(0, 0, 0, 0);
         pushButton = new QPushButton(centralwidget);
         pushButton->setObjectName(QString::fromUtf8("pushButton"));
-        pushButton->setGeometry(QRect(270, 230, 131, 61));
+        pushButton->setGeometry(QRect(40, 30, 131, 61));
         pushButton->setAutoDefault(true);
+        buttonBox = new QDialogButtonBox(centralwidget);
+        buttonBox->setObjectName(QString::fromUtf8("buttonBox"));
+        buttonBox->setGeometry(QRect(30, 160, 192, 28));
+        buttonBox->setStandardButtons(QDialogButtonBox::Cancel|QDialogButtonBox::Ok);
+        widget = new QWidget(centralwidget);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        widget->setGeometry(QRect(40, 220, 231, 111));
+        pushButton_8 = new QPushButton(widget);
+        pushButton_8->setObjectName(QString::fromUtf8("pushButton_8"));
+        pushButton_8->setGeometry(QRect(0, 10, 93, 28));
+        pushButton_9 = new QPushButton(widget);
+        pushButton_9->setObjectName(QString::fromUtf8("pushButton_9"));
+        pushButton_9->setGeometry(QRect(120, 10, 93, 28));
+        pushButton_10 = new QPushButton(widget);
+        pushButton_10->setObjectName(QString::fromUtf8("pushButton_10"));
+        pushButton_10->setGeometry(QRect(0, 60, 93, 28));
+        pushButton_11 = new QPushButton(widget);
+        pushButton_11->setObjectName(QString::fromUtf8("pushButton_11"));
+        pushButton_11->setGeometry(QRect(120, 60, 93, 28));
+        lineEdit = new QLineEdit(centralwidget);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+        lineEdit->setGeometry(QRect(230, 100, 113, 21));
+        comboBox = new QComboBox(centralwidget);
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->setObjectName(QString::fromUtf8("comboBox"));
+        comboBox->setGeometry(QRect(50, 340, 87, 22));
+        labelText = new QLabel(centralwidget);
+        labelText->setObjectName(QString::fromUtf8("labelText"));
+        labelText->setGeometry(QRect(580, 70, 68, 15));
+        labelimage = new QLabel(centralwidget);
+        labelimage->setObjectName(QString::fromUtf8("labelimage"));
+        labelimage->setGeometry(QRect(800, 60, 121, 101));
+        labelimage->setScaledContents(true);
+        labelGif = new QLabel(centralwidget);
+        labelGif->setObjectName(QString::fromUtf8("labelGif"));
+        labelGif->setGeometry(QRect(560, 230, 161, 131));
+        labelUrl = new QLabel(centralwidget);
+        labelUrl->setObjectName(QString::fromUtf8("labelUrl"));
+        labelUrl->setGeometry(QRect(810, 230, 121, 71));
+        change = new QPushButton(centralwidget);
+        change->setObjectName(QString::fromUtf8("change"));
+        change->setGeometry(QRect(270, 30, 93, 28));
+        lcdNumber = new QLCDNumber(centralwidget);
+        lcdNumber->setObjectName(QString::fromUtf8("lcdNumber"));
+        lcdNumber->setGeometry(QRect(660, 410, 121, 71));
+        progressBar = new QProgressBar(centralwidget);
+        progressBar->setObjectName(QString::fromUtf8("progressBar"));
+        progressBar->setGeometry(QRect(290, 480, 191, 31));
+        progressBar->setValue(24);
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 800, 26));
+        menubar->setGeometry(QRect(0, 0, 996, 26));
+        menu = new QMenu(menubar);
+        menu->setObjectName(QString::fromUtf8("menu"));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
         MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menu->menuAction());
+        menu->addAction(actionnew);
+        menu->addSeparator();
 
         retranslateUi(MainWindow);
         QObject::connect(pushButton, SIGNAL(clicked()), MainWindow, SLOT(close()));
@@ -65,7 +139,22 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        actionnew->setText(QCoreApplication::translate("MainWindow", "new", nullptr));
         pushButton->setText(QCoreApplication::translate("MainWindow", "Close", nullptr));
+        pushButton_8->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+        pushButton_9->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+        pushButton_10->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+        pushButton_11->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+        lineEdit->setText(QCoreApplication::translate("MainWindow", "123", nullptr));
+        comboBox->setItemText(0, QCoreApplication::translate("MainWindow", "heh", nullptr));
+        comboBox->setItemText(1, QCoreApplication::translate("MainWindow", "1234", nullptr));
+
+        labelText->setText(QCoreApplication::translate("MainWindow", "TextLabel", nullptr));
+        labelimage->setText(QCoreApplication::translate("MainWindow", "TextLabel", nullptr));
+        labelGif->setText(QCoreApplication::translate("MainWindow", "TextLabel", nullptr));
+        labelUrl->setText(QCoreApplication::translate("MainWindow", "TextLabel", nullptr));
+        change->setText(QCoreApplication::translate("MainWindow", "change", nullptr));
+        menu->setTitle(QCoreApplication::translate("MainWindow", "\346\226\207\344\273\266", nullptr));
     } // retranslateUi
 
 };
